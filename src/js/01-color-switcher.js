3a24bb0c9e6a1463091382dab6cd254594280e3a
okuzmychov <o.kuzmichev@gmail.com>
2023-07-06 12:09:47 +0300

    update-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 
+let intervalId = null;
+let currentColor = '';
+
+const startColorChange = () => {
+  startButton.disabled = true;
+  intervalId = setInterval(() => {
+    currentColor = getRandomHexColor();
+    document.body.style.backgroundColor = currentColor;
+  }, 1000);
+};
+
+const stopColorChange = () => {
+  startButton.disabled = false;
+  clearInterval(intervalId);
+};
+
+startButton.addEventListener('click', startColorChange);
+stopButton.addEventListener('click', stopColorChange);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
