114563bcfac8f1bf3ab72f95396007b09759ab4b
okuzmychov <125366168+okuzmychov@users.noreply.github.com>
2023-07-12 15:47:40 +0300

    Task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,52 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const delayInput = document.querySelector('input[name="delay"]');
+  const stepInput = document.querySelector('input[name="step"]');
+  const amountInput = document.querySelector('input[name="amount"]');
+
+  const delay = Number(delayInput.value);
+  const step = Number(stepInput.value);
+  const amount = Number(amountInput.value);
+
+  createPromises(amount, delay, step);
+}
+
+function createPromises(amount, initialDelay, step) {
+  for (let i = 1; i <= amount; i++) {
+    const position = i;
+    const delay = initialDelay + (i - 1) * step;
+
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.Success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.Failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
